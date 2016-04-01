ed940665c518482c619743793189e0537485add9
Vicky L <vicky.sy.loo@gmail.com>
2016-03-31 23:09:53 -0400

    attempt until titeize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -15,6 +15,30 @@ def start_of_word(word, number)
 end
 
 def first_word(string)
-  array = string.split(" ")
+  array = string.split(" ") # splits each word into an element in an array
   array[0]
+end
+#
+# def titleize(string)
+#   string.each do |word|
+#   word.capitalize unless ["and", "or", "over", "the"].include?(word)
+#   end
+# end
+
+# def titleize(string)
+#   new_array = string.split.map(",")
+#   result = new_array.each do |words|
+#     puts words
+#   end
+#   new_string = result.join
+#   new_string.(&:capitalize) unless ["and", "or", "over", "the"].include?(new_array)
+# end
+
+
+def titleize(string)
+  small_words = ["and"]
+  new_array = string.split(",")
+  new_array.map(&:capitalize) unless new_array.include?(small_words)
+
+  new_array.join(" ")
 end
